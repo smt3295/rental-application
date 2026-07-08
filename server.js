005aedd91cf87a71eb355632d8b1cab351bb7bac
smt3295 <smt3295@gmail.com>
2026-07-08 09:45:34 -0500

    Change static file serving directory to root

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
