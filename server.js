e7aef2742d0016756cdcf72bb72e1ec993eb067f
smt3295 <smt3295@gmail.com>
2026-07-08 13:35:55 -0500

    Serve index.html from the root route

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(helmet({ contentSecurityPolicy: false }));
 app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 function clean(value) {
   return String(value || '').trim();
